3c0d96c53f6e140a3d9073439032d68a496aacc0
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-10-25 14:40:57 +0300

    added footer design

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,10 @@
 <footer class="footer">
     <nav class="footer__navigation">
-        
+        <ul class="footer__navigation--item"><a href="#">Categories</a></ul>
+        <ul class="footer__navigation--item"><a href="#">Articles</a></ul>
+        <ul class="footer__navigation--item"><a href="#">Writers</a> </ul>
+        <ul class="footer__navigation--item"><a href="#">Contact Us</a></ul>
+        <ul class="footer__navigation--item"><a href="#">Tags</a></ul>
     </nav>
     <p>
         copyright

3e55181d7a493b8297b403017acfedfe762d000a
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-10-26 18:41:52 +0300

    added footer section
    added a footer section

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,22 @@
 <footer class="footer">
-    <nav class="footer__navigation">
-        <ul class="footer__navigation--item"><a href="#">Categories</a></ul>
-        <ul class="footer__navigation--item"><a href="#">Articles</a></ul>
-        <ul class="footer__navigation--item"><a href="#">Writers</a> </ul>
-        <ul class="footer__navigation--item"><a href="#">Contact Us</a></ul>
-        <ul class="footer__navigation--item"><a href="#">Tags</a></ul>
-    </nav>
+    <div class="footer__wrapper">
+
+        <nav class="footer__navigation">
+            <ul class="footer__navigation--item"><a href="#">Categories</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Articles</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Writers</a> </ul>
+            <ul class="footer__navigation--item"><a href="#">Contact Us</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Tags</a></ul>
+        </nav>
+
+        <nav class="footer__navigation">
+            <ul class="footer__navigation--item"><a href="#">Categories</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Articles</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Writers</a> </ul>
+            <ul class="footer__navigation--item"><a href="#">Contact Us</a></ul>
+            <ul class="footer__navigation--item"><a href="#">Tags</a></ul>
+        </nav>
+    </div>
     <p>
         copyright
         <a href="#">Anguzu Daniel</a>.

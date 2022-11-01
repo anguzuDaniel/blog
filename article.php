970054a971b1243e443ea664ade4bf0e1fcfe925
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2022-11-01 19:51:45 +0300

    changed the article page layout
    changed the article page layout

--- article.php
+++ article.php
@@ -28,40 +28,42 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
 
 <div class="article">
 
-    <?php if ($article === null) : ?>
-        <p class="paragraph article--paragraph">No articles found..</p>
-    <?php else : ?>
-        <article>
-            <div class="article--image">
-                <img src="images/<?= $article['article_image']; ?>" alt="article image" />
-            </div>
+    <div>
+        <?php if ($article === null) : ?>
+            <p class="paragraph article--paragraph">No articles found..</p>
+        <?php else : ?>
+            <article>
+                <div class="article--image">
+                    <img src="images/<?= $article['article_image']; ?>" alt="article image" />
+                </div>
 
-            <div class="article--text">
-                <h1 class="article--title"><?= htmlspecialchars($article['article_title']); ?></h1>
-                <p class="article--paragraph"><?= htmlspecialchars($article['article_content']); ?></p>
-            </div>
+                <div class="article--text">
+                    <h1 class="article--title"><?= htmlspecialchars($article['article_title']); ?></h1>
+                    <p class="article--paragraph"><?= htmlspecialchars($article['article_content']); ?></p>
+                </div>
 
-            <hr>
+                <hr>
 
-            <div class="article__comments">
-                <p class="article__comments--no">No comments yet..</p>
-            </div>
+                <div class="article__comments">
+                    <p class="article__comments--no">No comments yet..</p>
+                </div>
 
-            <form class="article--comments" action="" method="post">
-                <h1 class="article--title">Please leave a comment</h1>
-                <!-- <div class="form__row">
+                <form class="article--comments" action="" method="post">
+                    <h1 class="article--title">Please leave a comment</h1>
+                    <!-- <div class="form__row">
                     <label for="username" class="form__row--label">Username</label>
                     <input type="text" name="username" id="">
                 </div> -->
-                <div class="form__row">
-                    <label for="comment" class="form__row--label">Comment</label>
-                    <textarea name="comment" id="" cols="30" rows="10" style="resize: none;"></textarea>
-                </div>
-                <input type="submit" value="comment" class="btn | btn--submit">
-            </form>
-        </article>
+                    <div class="form__row">
+                        <label for="comment" class="form__row--label">Comment</label>
+                        <textarea name="comment" id="" cols="30" rows="10" style="resize: none;"></textarea>
+                    </div>
+                    <input type="submit" value="comment" class="btn | btn--submit">
+                </form>
+            </article>
 
-    <?php endif ?>
+        <?php endif ?>
+    </div>
 
     <?php require_once "includes/side.php" ?>
 </div>
